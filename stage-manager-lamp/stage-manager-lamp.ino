560a4fe5d0fe515759e5a9c519944ee4fd6d4707
Gabriel Landes <gabe.landes@fortra.com>
2024-03-17 09:51:39 -0500

    Add sketch

---
+++ stage-manager-lamp/stage-manager-lamp.ino
@@ -0,0 +1,26 @@
+const int powerPin = 3;
+const int redPin = 0;
+const int whitePin = 1;
+const int levelPin = A1;
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(whitePin, OUTPUT);
+  pinMode(powerPin, OUTPUT);
+  digitalWrite(powerPin, HIGH); 
+}
+void loop() {
+  int levelVal = map(analogRead(levelPin), 0, 1023, 0, 510);
+  int rVal = getRedVal(levelVal);
+  int wVal = getWhiteVal(levelVal);
+  analogWrite(redPin, rVal);
+  analogWrite(whitePin, wVal);
+  delay(1);
+}
+int getRedVal(int raw) {
+  if (raw < 255) return raw;
+  else return max(510 - raw, 0);
+}
+int getWhiteVal(int raw) {
+  raw = raw - 255;
+  return max(raw, 0);
+}
